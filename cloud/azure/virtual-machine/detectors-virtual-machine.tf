c6455d3648979af2a512ca3a67e9fe0960d98dc6
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2020-07-29 15:57:05 +0200

    Azure: Virtual Machine related detectors fixes

--- cloud/azure/virtual-machine/detectors-virtual-machine.tf
+++ cloud/azure/virtual-machine/detectors-virtual-machine.tf
@@ -3,7 +3,8 @@ resource "signalfx_detector" "heartbeat" {
 
   program_text = <<-EOF
         from signalfx.detectors.not_reporting import not_reporting
-        signal = data('Percentage CPU', filter=filter('resource_type', 'Microsoft.Compute/virtualMachines') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom}).publish('signal')
+        base_filter = filter('resource_type', 'Microsoft.Compute/virtualMachines') and filter('primary_aggregation_type', 'true')
+        signal = data('Percentage CPU', filter=base_filter and ${module.filter-tags.filter_custom}).publish('signal')
         not_reporting.detector(stream=signal, resource_identifier=['azure_resource_id'], duration='${var.heartbeat_timeframe}').publish('CRIT')
     EOF
 
@@ -21,13 +22,14 @@ resource "signalfx_detector" "cpu_usage" {
   name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure Virtual Machine CPU usage"
 
   program_text = <<-EOF
-        signal = data('Percentage CPU', filter=filter('resource_type', 'Microsoft.Compute/virtualMachines') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.cpu_usage_aggregation_function}.${var.cpu_usage_transformation_function}(over='${var.cpu_usage_transformation_window}').publish('signal')
-        detect(when(signal > ${var.cpu_usage_threshold_critical})).publish('CRIT')
-        detect(when(signal > ${var.cpu_usage_threshold_warning}) and when(signal <= ${var.cpu_usage_threshold_critical})).publish('WARN')
+        base_filter = filter('resource_type', 'Microsoft.Compute/virtualMachines') and filter('primary_aggregation_type', 'true')
+        signal = data('Percentage CPU', filter=base_filter and ${module.filter-tags.filter_custom})${var.cpu_usage_aggregation_function}.publish('signal')
+        detect(when(signal > threshold(${var.cpu_usage_threshold_critical}), lasting="${var.cpu_usage_timer}")).publish('CRIT')
+        detect(when(signal > threshold(${var.cpu_usage_threshold_warning}), lasting="${var.cpu_usage_timer}") and when(signal <= ${var.cpu_usage_threshold_critical})).publish('WARN')
     EOF
 
   rule {
-    description           = "is too high > ${var.cpu_usage_threshold_critical}"
+    description           = "is too high > ${var.cpu_usage_threshold_critical}%"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.cpu_usage_disabled_critical, var.cpu_usage_disabled, var.detectors_disabled)
@@ -36,7 +38,7 @@ resource "signalfx_detector" "cpu_usage" {
   }
 
   rule {
-    description           = "is too high > ${var.cpu_usage_threshold_warning}"
+    description           = "is too high > ${var.cpu_usage_threshold_warning}%"
     severity              = "Warning"
     detect_label          = "WARN"
     disabled              = coalesce(var.cpu_usage_disabled_warning, var.cpu_usage_disabled, var.detectors_disabled)
@@ -46,18 +48,19 @@ resource "signalfx_detector" "cpu_usage" {
 }
 
 resource "signalfx_detector" "credit_cpu" {
-  name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure Virtual Machine remaining CPU credit %"
+  name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Azure Virtual Machine remaining CPU credit"
 
   program_text = <<-EOF
-        A = data('CPU Credits Remaining', filter=filter('resource_type', 'Microsoft.Compute/virtualMachines') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.credit_cpu_aggregation_function}
-        B = data('CPU Credits Consumed', filter=filter('resource_type', 'Microsoft.Compute/virtualMachines') and filter('primary_aggregation_type', 'true') and ${module.filter-tags.filter_custom})${var.credit_cpu_aggregation_function}
-        signal = ((A/(A+B))*100).fill(100).${var.credit_cpu_transformation_function}(over='${var.credit_cpu_transformation_window}').publish('signal')
-        detect(when(signal < ${var.credit_cpu_threshold_critical})).publish('CRIT')
-        detect(when(signal < ${var.credit_cpu_threshold_warning}) and when(signal >= ${var.credit_cpu_threshold_critical})).publish('WARN')
+        base_filter = filter('resource_type', 'Microsoft.Compute/virtualMachines') and filter('primary_aggregation_type', 'true')
+        A = data('CPU Credits Remaining', filter=base_filter and ${module.filter-tags.filter_custom})${var.credit_cpu_aggregation_function}
+        B = data('CPU Credits Consumed', filter=base_filter and ${module.filter-tags.filter_custom})${var.credit_cpu_aggregation_function}
+        signal = ((A/(A+B))*100).fill(100).${var.credit_cpu_transformation_function}(over='${var.credit_cpu_timer}').publish('signal')
+        detect(when(signal < threshold(${var.credit_cpu_threshold_critical}), lasting="${var.credit_cpu_timer}")).publish('CRIT')
+        detect(when(signal < threshold(${var.credit_cpu_threshold_warning}), lasting="${var.credit_cpu_timer}") and when(signal >= ${var.credit_cpu_threshold_critical})).publish('WARN')
     EOF
 
   rule {
-    description           = "is too low < ${var.credit_cpu_threshold_critical}"
+    description           = "is too low < ${var.credit_cpu_threshold_critical}%"
     severity              = "Critical"
     detect_label          = "CRIT"
     disabled              = coalesce(var.credit_cpu_disabled_critical, var.credit_cpu_disabled, var.detectors_disabled)
@@ -66,7 +69,7 @@ resource "signalfx_detector" "credit_cpu" {
   }
 
   rule {
-    description           = "is too low < ${var.credit_cpu_threshold_warning}"
+    description           = "is too low < ${var.credit_cpu_threshold_warning}%"
     severity              = "Warning"
     detect_label          = "WARN"
     disabled              = coalesce(var.credit_cpu_disabled_warning, var.credit_cpu_disabled, var.detectors_disabled)
