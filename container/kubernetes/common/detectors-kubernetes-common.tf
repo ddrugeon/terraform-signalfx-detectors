1cca21d89cef3e5c638b2a4412432849565e4932
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-09-01 18:18:46 +0200

    heartbeat comes back

--- container/kubernetes/common/detectors-kubernetes-common.tf
+++ container/kubernetes/common/detectors-kubernetes-common.tf
@@ -1,3 +1,22 @@
+resource "signalfx_detector" "heartbeat" {
+  name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Kubernetes node heartbeat"
+
+  program_text = <<-EOF
+    from signalfx.detectors.not_reporting import not_reporting
+    signal = data('kubernetes.node_ready', filter=${module.filter-tags.filter_custom}.mean(by=['kubernetes_cluster']).publish('signal')
+    not_reporting.detector(stream=signal, resource_identifier=['kubernetes_cluster'], duration='${var.heartbeat_timeframe}').publish('CRIT')
+EOF
+
+  rule {
+    description           = "has not reported in ${var.heartbeat_timeframe}"
+    severity              = "Critical"
+    detect_label          = "CRIT"
+    disabled              = coalesce(var.heartbeat_disabled, var.detectors_disabled)
+    notifications         = coalescelist(var.heartbeat_notifications, var.notifications)
+    parameterized_subject = "[{{ruleSeverity}}]{{{detectorName}}} {{{ruleName}}} on {{{dimensions}}}"
+  }
+}
+
 resource "signalfx_detector" "node_ready" {
   name = "${join("", formatlist("[%s]", var.prefixes))}[${var.environment}] Kubernetes node status"
 
